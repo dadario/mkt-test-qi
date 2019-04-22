65d580963b06b7195e4d138b7780a027f82ee8ba
Adriano Dadario <adriano.dadario@vagas.com.br>
2019-04-22 12:00:16 -0300

    Adjusting code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :questions
+  resources :services do
+    collection do
+      post :requested
+    end
+  end
+  resources :answers, only: :create
+
+  get '/finished', to: 'process#finished'
+  get '/:external_code', to: 'process#index'
 end
