106f149709fa8af1bb39396c60272dc99bf3b4b6
Adriano Dadario <adriano.dadario@vagas.com.br>
2019-04-25 16:50:00 -0300

    Fixed a host for email

--- app/services/external/delivery_message.rb
+++ app/services/external/delivery_message.rb
@@ -9,7 +9,7 @@ module External
          Você foi convidado pela empresa #{service.company_name} à participar
         de um teste de QI para a vaga de "#{service.job_title}"
         </pre>
-         <a href="http://localhost:3000/#{service.external_code}">Vamos lá?</a>
+         <a href="https://mkt-test-app.herokuapp.com/#{service.external_code}" target="_blank">Vamos lá?</a>
       MAIL
 
       attributes = {
@@ -21,7 +21,7 @@ module External
       path = "/v1/oasis-portal/requests/#{service.external_code}/messages"
       Typhoeus.post("#{ENV['GATEWAY_HOST']}#{path}",
                     body: attributes.to_json,
-                    headers: { 'Content-type' => 'application/json',
+                    headers: { 'Content-Type' => 'application/json',
                                'Authorization' => "Bearer #{access_token}" })
     end
   end
