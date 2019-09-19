c206ef6ec4d48dafaf2c9990f182df4bd312dba9
Adriano Dadario <adriano.dadario@vagas.com.br>
2019-09-19 15:40:19 -0300

    Apresentação de decimal value com até duas casas

--- app/models/service.rb
+++ app/models/service.rb
@@ -13,7 +13,7 @@ class Service < ApplicationRecord
     total_questions = Question.count
     right_answers = answers.select(&:corret?).size
     {
-      score: ((right_answers * 100) / total_questions) * 1.0,
+      score: ((right_answers * 100.0) / total_questions).round(2),
       summary: 'A simple description about this test',
       link: 'http://localhost:3000/link.pdf',
       completionDate: created_at.strftime("%Y-%m-%d"),
