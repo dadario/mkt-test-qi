0f9be881468fbd8509dcbf81e470478ba08dbfba
Adriano Dadario <adriano.dadario@vagas.com.br>
2019-10-17 15:44:15 -0300

    Atualização do modelo do e-mail

--- app/services/external/delivery_message.rb
+++ app/services/external/delivery_message.rb
@@ -4,12 +4,30 @@ module External
   module DeliveryMessage
     def send_message
       mail_body = <<~MAIL
-        <pre>
-        Olá #{service.applicant_name},
-         Você foi convidado pela empresa #{service.company_name} à participar
-        de um teste de QI para a vaga de "#{service.job_title}"
-        </pre>
-         <a href="https://mkt-test-app.herokuapp.com/#{service.external_code}" target="_blank">Vamos lá?</a>
+<p>
+  Olá #{service.applicant_name},
+</p>
+<p>
+  Você foi convidado(a) para participar de um processo de avaliação: <br />
+  <b>Descrição da Oportunidade:</b> #{service.job_title}
+</p>
+<p>
+  Para participar, basta se cadastrar, aceitar o convite e fazer sua avaliação.<br />
+  É rápido e muito simples!
+</p>
+<p>
+  <a href="https://mkt-test-app.herokuapp.com/#{service.external_code}" target="_blank">Clique aqui!</a>
+</p>
+<p>
+  Sucesso!<br />
+  Equipe JobCoach Empresas
+</p>
+<p>
+  Atenção: Você só estará participando deste processo se Aceitar o convite no Painel do JobCoach Empresas.
+</p>
+<small>
+  Esta é uma mensagem automática. Por favor, não responda este e-mail.
+</small>
       MAIL
 
       attributes = {
